eeb2b798c59522f88d07a988c92b9352dee5f247
iamgymie <6257063+gymie@users.noreply.github.com>
2023-11-02 11:02:29 +0700

    add navigate and currency

--- src/ProdukDetail.jsx
+++ src/ProdukDetail.jsx
@@ -1,15 +1,17 @@
 import data from "./data.json";
-import { useParams  } from "react-router-dom";
+import { useParams,useNavigate  } from "react-router-dom";
 
 export default function ProdukDetail(){
     const {id} = useParams();
+    const navigate = useNavigate();
 
     const produk = data.find((item) => item.id === parseInt(id));
     return (
         <>
             <h1>Detail Produk {produk.nama_produk}</h1>
             <p>Jenis: {produk.jenis}</p>
-            <p>Harga: Rp. {produk.harga}</p>
+            <p>Harga: Rp {produk.harga.toLocaleString('id-ID')}</p>
+            <button onClick={()=>navigate(-1)}>Back</button>
         </>
     )
 }
